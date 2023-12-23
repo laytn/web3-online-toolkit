6eac2721fbd58b2294147b4775332c46676ff957
laytn <pwh7121@gmail.com>
2023-12-23 21:56:43 +0900

    refactor: 데이터 스토어 분리

---
+++ src/stores/commonData.ts
@@ -0,0 +1,7 @@
+export const MNEMONIC_LANGAGE = ["English", "Korean"]
+export const DIGIT = ["12", "24"]
+export const MNEMONIC_PATH: { [key: string]: number } = {
+    "ETHEREUM": 60,
+    "POLYGON": 966,
+    "KLAYTN": 8217,
+}
--- src/stores/evmData.ts
+++ src/stores/evmData.ts
@@ -1,4 +1,2 @@
 export const EVM_CHAINS = ["Ethereum", "Polygon", "Klaytn"];
 export const EVM_FUNCS = ["unitConvert", "checksumAddress", "mnemonicConverter"];
-export const MNEMONIC_LANGAGE = ["English", "Korean"]
-export const DIGIT = ["12", "24"]
