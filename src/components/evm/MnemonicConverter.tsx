3c9ea54775a79f2a93207bf216488232e2b6da0a
laytn <ethan@ahnlab.io>
2024-06-25 09:33:06 +0900

    feat: mnemonic converter 퍼블릭키, 주소 노출 추가

--- src/components/evm/MnemonicConverter.tsx
+++ src/components/evm/MnemonicConverter.tsx
@@ -1,10 +1,11 @@
 import { Box, Grid, SelectChangeEvent } from "@mui/material"
-import Header from "../Header"
 import { DIGIT, MNEMONIC_LANGAGE } from "../../stores/commonData"
 import React, { useState } from "react"
-import useMnemonicGenerator from "../../hooks/useMnemonicGenerator"
+
+import Header from "../Header"
 import SelectBox from "../SelectBox"
 import SubTitle from "../vac/SubTitle"
+import useMnemonicGenerator from "../../hooks/useMnemonicGenerator"
 
 const centerFlex: React.CSSProperties = { display: "flex", alignItems: "center", justifyContent: "center" }
 const titleStyle: React.CSSProperties = { ...centerFlex, fontSize: "20px" }
@@ -14,7 +15,7 @@ const MnemonicConverter = () => {
     const [langage, setLangage] = useState<string>(MNEMONIC_LANGAGE[0])
     const [digit, setDigit] = useState<string>(DIGIT[0])
 
-    const { mnemonic, privateKey } = useMnemonicGenerator(digit, langage)
+    const { mnemonic, publicKey, privateKey, address } = useMnemonicGenerator(digit, langage)
 
     const handleLangageChange = (event: SelectChangeEvent<string>) => {
         setLangage(event.target.value);
@@ -45,10 +46,22 @@ const MnemonicConverter = () => {
                     {mnemonic}
                 </Box>
                 <Box sx={titleStyle}>
+                    public key
+                </Box>
+                <Box sx={contentStyle}>
+                    {publicKey}
+                </Box>
+                <Box sx={titleStyle}>
                     private key
                 </Box>
                 <Box sx={contentStyle}>
                     {privateKey}
+                </Box>
+                <Box sx={titleStyle}>
+                    address
+                </Box>
+                <Box sx={contentStyle}>
+                    {address}
                 </Box>
             </Grid>
         </Grid >
